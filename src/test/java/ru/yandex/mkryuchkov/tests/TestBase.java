93d843b21772146112613e26a8e2267b66953d51
Максим <Raboznik@yandex.ru>
2022-04-12 22:24:23 +0300

    add few tests

--- src/test/java/ru/yandex/mkryuchkov/tests/TestBase.java
+++ src/test/java/ru/yandex/mkryuchkov/tests/TestBase.java
@@ -32,7 +32,8 @@ public class TestBase {
         String login = System.getProperty("login", cfgs.remoteLogin());
         String pass = System.getProperty("pass", cfgs.remotePassword());
 
-        Configuration.remote = "https://" + login + ":" + pass + "@" + remoteUrl;
+//        Configuration.remote = "https://" + login + ":" + pass + "@" + remoteUrl;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.browser = browser;
         Configuration.browserVersion = version;
         Configuration.browserSize = size;
